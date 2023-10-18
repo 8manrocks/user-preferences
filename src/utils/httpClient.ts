56a685c36af5a04d809d93151a4739553c10e787
8man <8manrocks@gmail.com>
2023-10-18 05:57:51 +0530

    k

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,7 +1,7 @@
 // api.js
 import axios from "axios";
 import { Constants } from "./constants";
-
+axios.defaults.withCredentials = true;
 const API = axios.create({
   baseURL: Constants.BASE_URL,
 });
